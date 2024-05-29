05d2a1b6dc3bbdaa315e7a2adf70c63e491e189a
anaprr <anapaularamos20101@hotmail.com>
2024-05-29 09:54:53 -0300

    att do app

--- src/Pages/Busca.js
+++ src/Pages/Busca.js
@@ -1,28 +1,131 @@
-import { View, Text, TextInput, StyleSheet, FlatList, ActivityIndicator } from 'react-native';
+import { View, Text, TextInput, StyleSheet, FlatList, ActivityIndicator, Alert, TouchableOpacity } from 'react-native';
 import React, { useEffect, useState } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
 
 export default function Busca() {
     const [usuarios, setUsuarios ] = useState( [] );
+    const [clientes, setClientes ] = useState( [] );
+    const [edicao, setEdicao] = useState(false);
     const [error, setError ] = useState(false);
     const [busca, setBusca] = useState(false);
     const [filtro, setFiltro ] = useState(false);
+    const [clientId, setclientId] = useState(0);
+    const [clientNome, setNome] = useState();
+    const [clientEmail, setEmail] = useState();
+    const [clientGenere, setGenere] = useState();
+    const [deleteResposta, setResposta] = useState(false);
 
-    async function getUsuarios()
+    async function getClientes()
     {
-        await fetch('https://fakestoreapi.com/users', {
+        await fetch('http://10.139.75.21:5251/api/Clients/GetAllClients', {
             method: 'GET',
             headers: {
               'content-type': 'application/json'
             }
           })
-            .then( res => ( res.ok == true ) ? res.json() : false )
-            .then( json => setUsuarios( json ) )
-            .catch( err => setError( true ) )
+          .then( res => res.json())
+          .then(json => setClientes(json))
+          .catch(err => setError(true))
     }
 
+    async function getClients(id)
+    {    
+      await fetch('http://10.139.75.21:5251/api/Clients/GetClientId/' + id,{
+              method: 'GET',
+              headers: {
+                  'Content-type' : 'application/json; charset=UTF-8',
+              },
+          })
+          .then((response)=> response.json())        
+          .then(json=>{
+            setclientId(json.clientId);
+            setNome(json.clientName);
+            setEmail(json.clientEmail);
+            setGenere(json.clientGenere);
+          });
+    }
+
+    async function editClient()
+  {    
+    await fetch('http://10.139.75.41:5251/api/Clients/UpdateClient/' + clientId,{
+            method: 'PUT',
+            headers: {
+                'Content-type' : 'application/json; charset=UTF-8',
+            },
+            body: JSON.stringify({
+                clientId : clientId,
+                clientName : clientNome,
+                clientEmail : clientEmail,
+                clientGenere : clientGenere,
+
+            })
+        })
+        .then( (response) => response.json())
+        .catch(err => console.log(err));
+        getClientes();
+        setEdicao(false);
+  }
+
+  function showAlert(id, clientName) {
+    Alert.alert(
+      '',
+      'Deseja realmente excluir esse cliente?',
+      [
+        { text: 'Sim', onPress: () => deleteClient(id, clientName) },
+        { text: 'Não', onPress: () => ('') }
+      ],
+      { cancelable: false }
+    );
+  }
+
+  async function deleteClient(id, clientName) {
+    await fetch('http://10.139.75.21:5251/api/Clients/DeleteClient/' + id,{
+            method: 'DELETE',
+            headers: {
+                'Content-type' : 'application/json; charset=UTF-8',
+            },
+        })
+        .then(res => res.json())
+        .then(json => setResposta(json))
+        .catch(err => setError(true))
+
+        if(deleteResposta == true)
+          {
+            Alert.alert(
+              '',
+              'Cliente' + clientName + 'não foi excluido com sucesso',
+              [
+                { text: '', onPress: () => ('')},
+                { text: 'Ok', onPress: () => ('')},
+              ],
+              { cancelable: false}
+            );
+            getClientes();
+          }
+          else{
+            Alert.alert(
+              '',
+              'Cliente' + clientName + 'foi excluido com sucesso',
+              [
+                { text: '', onPress: () => ('')},
+                { text: 'Ok', onPress: () => ('')},
+              ],
+              { cancelable: false}
+            );
+            getClientes();
+          }
+
+  }
+
     useEffect( () => {
-        getUsuarios();
+        getClientes();
     }, [] );
+
+    useFocusEffect(
+        React.useCallback(()=>{
+          getClientes();
+        },[])
+      );
 
     useEffect( () => {
         setFiltro( usuarios.filter( (item) => item.name.firstname == busca )[0] );
@@ -30,43 +133,76 @@ export default function Busca() {
 
     return (
         <View style={css.container}>
-            <View style={css.searchBox}>
-                <TextInput
-                    style={css.search}
-                    placeholder="Buscar usuarios"
-                    placeholderTextColor="white"
-                    TextInput={busca}
-                    onChangeText={(digitado) => setBusca( digitado ) }
-                />
-            </View>
-            { filtro && <Text style={css.text}>{filtro.name.firstname} {filtro.name.lastname}</Text> }
-            { ( !filtro && busca ) && <ActivityIndicator size="large" color="white" /> }
-        </View>
-    )
-}
+            <View style={css.container}>
+                {edicao == false ?
+                <FlatList
+                style={css.flat}
+                data={clientes}
+                keyExtractor={(item) => item.clientId}
+                renderItem={({item})=>(
+                    <Text style={css.text}>
+                        {item.clientName}
+                        <TouchableOpacity style={css.btnEdit} onPress={() => {setEdicao(true); getClients(item.clientId)}}>
+                            <Text style={css.btnLoginText}>EDITAR   </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={css.btnDelete} onPress={()=> showAlert(item.clientId, item.clientName)}>
+                            <Text style={css.btnLoginText}>EXCLUIR</Text>
+                        </TouchableOpacity>
+                    </Text>
+                )}
+            />
+
+        :
+        <View style={css.editar}>
+        <TextInput
+            inputMode="text"
+            style={css.input}
+            value={clientNome}
+            onChangeText={(digitado)=> setNome(digitado)}
+            placeholderTextColor="black"   
+        />
+        <TextInput
+            inputMode="email"
+            style={css.input}
+            value={clientEmail}
+            onChangeText={(digitado)=> setEmail(digitado)}
+            placeholderTextColor="black"   
+        />
+        <TextInput
+            inputMode="text"
+            secureTextEntry={true}
+            style={css.input}
+            value={clientGenere}
+            onChangeText={(digitado)=> setGenere(digitado)}
+            placeholderTextColor="black"    
+        />
+        <TouchableOpacity style={css.btnCreate} onPress={()=>editClient()}>
+            <Text styel={css.btnLoginText}>SALVAR</Text>
+        </TouchableOpacity>
+    </View>
+
+        }
+    </View>
+    </View>
+
+    )}
+        
 const css = StyleSheet.create({
-    container: {
+    container:{
         flexGrow: 1,
-        width: "100%",
-        alignItems: "center",
-        backgroundColor: "#191919",
-    },
-    text: {
-        color: "white"
-    },
-    searchBox: {
-        width: "100%",
-        height: 100,
-        justifyContent: "flex-end",
-        alignItems: "center",
-    },
-    search: {
-        width: "96%",
-        height: 60,
+        backgroundColor:'#D8ADF2',
+        alignItems:'center',
+        marginTop: 20
+      },
+      searchBox: {
+        width: "80%",
+        height: 50,
         borderWidth: 1,
-        borderColor: "white",
-        borderRadius: 8,
-        padding: 10,
-        color: "white"
-    }
+        borderRadius: 5,
+        padding: 15,
+        marginBottom: 25,
+        color:"white",
+        backgroundColor: "gray"
+    
+      }
 })
--- src/Pages/Inserir.js
+++ src/Pages/Inserir.js
@@ -1,22 +1,90 @@
-import { View, Text, StyleSheet} from 'react-native'
-import React from 'react'
+import { View,  Text, ScrollView, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 
 export default function Inserir() {
+    const[nome, setNome] = useState("");
+    const[email, setEmail] = useState("");
+    const[genere, setGenere] = useState("");
+    const[erro, setErro]=useState("");
+    const[sucesso, setSucesso]=useState("");
+
+    async function Cadastro(){
+      await fetch('https://fakestoreapi.com/users',{
+          method: 'POST',
+          headers:{
+              'content-type' : 'application/json'
+          },
+          body:JSON.stringify(
+              {
+                nome:nome,
+                email:email,
+                genere:genere,
+                  name:{
+                      firstname:nome
+                  },
+              }
+          )
+      })
+      .then( res => (res.ok == true) ? res.json () : false)
+      .then(json => {
+          setSucesso((json.id) ? true : false);
+          setErro((json.id) ? false : true);
+
+      } )
+      .catch(err => setErro(true))
+  }
+
+
   return (
-    <View style={css.container}>
-      <Text style={css.text}>Inserir</Text>
-    </View>
+    <ScrollView contentContainerStyle={css.container}>
+    { sucesso ? 
+        <Text>Obrigada por se cadastrar</Text>
+    :
+    <>
+    <TextInput style={css.texto} placeholder='Nome'TextInput={nome} onChangeText={(digitado) => setNome(digitado)}></TextInput>
+    <TextInput style={css.texto} placeholder='Genere' TextInput={genere} onChangeText={(digitado) => setGenere(digitado)}></TextInput>
+     <TextInput style={css.texto} placeholder='Email'TextInput={email} onChangeText={(digitado) => setEmail(digitado)}></TextInput>
+    <TouchableOpacity style={css.btn} onPress={Cadastro}>
+      <Text style={css.btnText}>Cadastrar</Text>
+    </TouchableOpacity>
+    {erro && <Text>Revise os campos e Tente novamente</Text>}
+    </>
+    
+}
+
+</ScrollView>
   )
 }
 const css = StyleSheet.create({
-  container: {
-    backgroundColor: "#191919",
-    flexGrow: 1,
-    color: "white",
-    justifyContent: "center",
-    alignItems: "center"
+  container:{
+      backgroundColor:"#D8ADF2",
+      flexGrow: 1,
+      justifyContent:"center",
+      alignItems:"center"
   },
-  text: {
-    color: "white"
-  }
+  texto:{
+    width: "90%", 
+    height: 60,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: 'black',
+    padding: 15,
+    marginTop: 20,
+    marginBottom: 15,
+    
+  },
+  btn: {
+      backgroundColor: "white",
+      color:"black",
+      width: "70%",
+      height: 60,
+      borderRadius: 5,
+    },
+    btnText: {
+      color: "black",
+      textAlign: "center",
+      lineHeight: 60,
+      fontSize: 25, 
+      fontWeight: "bold"
+  },
 })
